a527caf90a8cec1c7ca35a429819562e4a1a21d0
Thomas Fysekis <thwmas321@gmail.com>
2022-01-07 14:36:13 +0200

    something like a "body" for getting started

---
+++ src/Account.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Account {
+    public String username(){
+        return null;
+    }
+
+    public int authToken(){
+
+        return 0;
+    }
+
+    public List<Message> messageBox(){
+
+        return null;
+    }
+
+
+}
---
+++ src/Message.java
@@ -0,0 +1,22 @@
+public class Message {
+
+    public boolean isRead(){
+
+        return false;
+    }
+
+    public String sender(){
+
+        return null;
+    }
+
+    public String receiver(){
+
+        return null;
+    }
+
+    public String body(){
+
+        return null;
+    }
+}
